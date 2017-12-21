77881a8b643a1c783737f66ee526638c00122396
flomock <florian_mock91@web.de>
2017-12-21 16:25:08 +0100

    fixed import a xml and output a json

--- nanocomb/load.py
+++ nanocomb/load.py
@@ -1,15 +1,14 @@
 from xmljson import badgerfish as bf
 from xml.etree.ElementTree import fromstring
-from xml.etree.ElementTree import Element, tostring
 from json import dumps
+import os
 
 def xml_to_json():
-
-    with open("/home/go96bix/projects/nanocomb/sampleData/HQ186308.xml","r") as file:
+    cwd = os.getcwd()
+    with open(cwd + "/examples/HQ186308.xml","r") as file:
         data = file.read()
-        print(data)
         output = bf.data(fromstring(data))
-        dumps(bf.data(output))
+        print(dumps(output))
 
 
 xml_to_json()
