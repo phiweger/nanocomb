a68490e489698fd9e6327130718f5d6bc47219d2
flomock <florian_mock91@web.de>
2017-12-21 16:41:40 +0100

    finished import a xml file and output a json file

--- nanocomb/load.py
+++ nanocomb/load.py
@@ -2,13 +2,23 @@ from xmljson import badgerfish as bf
 from xml.etree.ElementTree import fromstring
 from json import dumps
 import os
+from pathlib import Path
 
-def xml_to_json():
-    cwd = os.getcwd()
-    with open(cwd + "/examples/HQ186308.xml","r") as file:
-        data = file.read()
-        output = bf.data(fromstring(data))
-        print(dumps(output))
+def xml_to_json(path):
+    """
+    load xml file from path and parse to json
+    :param path:
+    :return:
+    """
+    file = Path(path)
+    if file.is_file():
+        with open(file, "r") as file:
+            data = file.read()
+            output = bf.data(fromstring(data))
+            return dumps(output)
+    else:
+        raise KeyError('no valid file path')
 
 
-xml_to_json()
+cwd = os.getcwd()
+xml_to_json(cwd + "/examples/HQ186308.xml")
